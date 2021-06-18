ad41be1c8a45ee8de4c9d8cc7abfec6d7ddab703
Takayuki Ashida <bobyan0818@gmail.com>
2021-06-18 18:13:06 +0900

    初回コミット

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.js-menu').click(function(){
+    $('.is-menu').fadeIn();
+  });
+  $('.js-menu-close').click(function(){
+    $('.is-menu').fadeOut();
+  });
+  $('.js-menu').click(function(){
+    $('.is-bg').fadeIn();
+  });
+  $('.js-menu-close').click(function(){
+    $('.is-bg').fadeOut();
+  });
+
+
+});
