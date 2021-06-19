f0c92d923ab20273fad2e7e63cc6d8af3ac4dd2f
Takayuki Ashida <bobyan0818@gmail.com>
2021-06-19 14:10:00 +0900

    add js removeAttr

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 $(function(){
+//   // 1. 変数mqlにMediaQueryListを格納
+// const mql = window.matchMedia('(min-width: 1100px)');
+
+// // 2. メディアクエリに応じて実行したい処理を関数として定義
+// const handleMediaQuery = function(mql) {
+//   if (mql.matches) {
+//     // 1100px以上の場合の処理
+//     $('h4').removeClass('js-menu');
+//     // console.log('1100px以上');
+//   } else {
+//     // 1100px未満の場合の処理
+//     $('h4').addClass('.js-menu');
+//     // console.log('1100px未満');
+//   }
+// };
   $('.js-menu').click(function(){
     $('.is-menu').fadeIn();
   });
+  // $('.js-menu-close').click(function(){
+  //   $('.is-menu').fadeOut();
+  // });
   $('.js-menu-close').click(function(){
-    $('.is-menu').fadeOut();
+    $('.is-menu').removeAttr('style');
   });
+  
   $('.js-menu').click(function(){
     $('.is-bg').fadeIn();
   });
   $('.js-menu-close').click(function(){
-    $('.is-bg').fadeOut();
+    $('.is-bg').fadeOut(200);
   });
 
 
