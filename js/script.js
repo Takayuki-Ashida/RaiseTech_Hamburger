f1b316f2f3051c85abc1145541dcc610a172bc6a
Takayuki Ashida <bobyan0818@gmail.com>
2021-06-24 13:41:40 +0900

    add load resize

--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
-$(function(){
-      $('.js-menu-close').click(function(){
-      $.when(
-        // 先に実行したい処理をここ
-        $('.is-menu').fadeOut()
-      
-      ).done(function(){ 
-        // その後実行したい処理をここ
-          $('.is-menu').removeAttr('style');
-      });
-    });
-    $('.js-menu').click(function(){
-      $('.is-menu').fadeIn();
-    });
+$(function () {
 
-    $(window).resize(function(){
-      var w = $(window).width();
-      var x = 1200;
-      if (w < x) {
-          // スマホ時の処理
-          $('.js-menu').click(function(){
-            $('.is-bg').fadeIn();
-          });
-      } else {
-          // ＰＣ時の処理
-          
-          
-      }return false
+  $(window).on('load resize', function () {
+    var w = $(window).width();
+    var x = 1200;
+    if (w <= x) {
+      $('#SideMenu').addClass('js-menu'),
+        $('.js-menu').click(function () {
+          $('.is-menu').fadeIn();
+        }),
+        $('.js-menu').click(function () {
+          $('.is-bg').fadeIn();
+        });
+    } else {
+      $('#SideMenu').removeClass('js-menu')
+    }
+  });
+
+
+  $('.js-menu-close').click(function () {
+    $.when(
+      // 先に実行したい処理をここ
+      $('.is-menu').fadeOut()
+
+    ).done(function () {
+      // その後実行したい処理をここ
+      $('.is-menu').removeAttr('style');
     });
-      
-    $('.js-menu-close').click(function(){
-      $('.is-bg').fadeOut();
-    });
+  });
+
+
+
+  $('.js-menu-close').click(function () {
+    $('.is-bg').fadeOut(),
+      $('.is-bg').removeAttr('style')
+  });
 });
